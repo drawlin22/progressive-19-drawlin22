e00b7def9e38b70aafb80cd173226bb19452e0d0
Donnie Rawlings <tacobellboi@yahoo.com>
2023-10-24 21:49:50 -0500

    Initialize files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # progressive-19-drawlin22
+
+
+
+
+Peer Colaboration:
+
+https://github.com/ColtonMakesStuff
---
+++ client/webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js',
+      }),
+      new WorkboxPlugin.GenerateSW()      
+    ],
+
+    module: {
+      rules: [
+        {
+        test: /\.css$i/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+        },
+      ],
+    },
+  };
+};
